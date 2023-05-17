a9331316d975178112bd6eb16e373bd6dd6e03f0
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-05-17 13:02:10 +0200

    Only png/jpg/jpeg for images

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -18,28 +18,31 @@ export default class NewBill {
 
   handleChangeFile = e => {
     e.preventDefault();
-    const file = this.document.querySelector('input[data-testid="file"]').files[0];
+    const inputField = this.document.querySelector('input[data-testid="file"]');
+    const file = inputField.files[0];
+    const fileNamePattern = /[A-z0-9]+.(png|jpg|jpeg)$/;
     const filePath = e.target.value.split(/\\/g);
     const fileName = filePath[filePath.length - 1];
-    const formData = new FormData();
-    const email = JSON.parse(localStorage.getItem('user')).email;
-    formData.append('file', file);
-    formData.append('email', email);
-
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({ fileUrl, key }) => {
-        console.log(fileUrl);
-        this.billId = key;
-        this.fileUrl = fileUrl;
-        this.fileName = fileName;
-      }).catch(error => console.error(error));
+    if (fileName.match(fileNamePattern)) {
+      inputField.setCustomValidity('');
+      const formData = new FormData();
+      const email = JSON.parse(localStorage.getItem('user')).email;
+      formData.append('file', file);
+      formData.append('email', email);
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true
+          }
+        })
+        .then(({ fileUrl, key }) => {
+          this.billId = key;
+          this.fileUrl = fileUrl;
+          this.fileName = fileName;
+        }).catch(error => console.error(error));
+    } else inputField.setCustomValidity('Invalid');
   };
 
   handleSubmit = e => {

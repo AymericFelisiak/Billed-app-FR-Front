caee5d5464b60ef3f42a79cbd686c08659d82dbc
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-05-25 02:46:39 +0200

    Fixed form. No longer add an empty bill when user just adds a file

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -13,6 +13,7 @@ export default class NewBill {
     this.fileUrl = null;
     this.fileName = null;
     this.billId = null;
+    this.inputFile = null;
     new Logout({ document, localStorage, onNavigate });
   }
 
@@ -25,9 +26,17 @@ export default class NewBill {
     const fileName = filePath[filePath.length - 1];
     if (fileType === 'image/png' || fileType === 'image/jpg' || fileType === 'image/jpeg') {
       inputField.setCustomValidity('');
-      const formData = new FormData();
-      const email = JSON.parse(localStorage.getItem('user')).email;
-      formData.append('file', file);
+      this.inputFile = file;
+      this.fileName = fileName;
+    } else inputField.setCustomValidity('Invalid');
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector('input[data-testid="datepicker"]').value);
+    const email = JSON.parse(localStorage.getItem('user')).email;
+    const formData = new FormData();
+      formData.append('file', this.inputFile);
       formData.append('email', email);
       this.store
         .bills()
@@ -40,30 +49,22 @@ export default class NewBill {
         .then(({ fileUrl, key }) => {
           this.billId = key;
           this.fileUrl = fileUrl;
-          this.fileName = fileName;
+          const bill = {
+            email,
+            type: e.target.querySelector('select[data-testid="expense-type"]').value,
+            name: e.target.querySelector('input[data-testid="expense-name"]').value,
+            amount: parseInt(e.target.querySelector('input[data-testid="amount"]').value),
+            date: e.target.querySelector('input[data-testid="datepicker"]').value,
+            vat: e.target.querySelector('input[data-testid="vat"]').value,
+            pct: parseInt(e.target.querySelector('input[data-testid="pct"]').value) || 20,
+            commentary: e.target.querySelector('textarea[data-testid="commentary"]').value,
+            fileUrl: this.fileUrl,
+            fileName: this.fileName,
+            status: 'pending'
+          };
+          this.updateBill(bill);
+          this.onNavigate(ROUTES_PATH.Bills);
         }).catch(error => console.error(error));
-    } else inputField.setCustomValidity('Invalid');
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector('input[data-testid="datepicker"]').value);
-    const email = JSON.parse(localStorage.getItem('user')).email;
-    const bill = {
-      email,
-      type: e.target.querySelector('select[data-testid="expense-type"]').value,
-      name: e.target.querySelector('input[data-testid="expense-name"]').value,
-      amount: parseInt(e.target.querySelector('input[data-testid="amount"]').value),
-      date: e.target.querySelector('input[data-testid="datepicker"]').value,
-      vat: e.target.querySelector('input[data-testid="vat"]').value,
-      pct: parseInt(e.target.querySelector('input[data-testid="pct"]').value) || 20,
-      commentary: e.target.querySelector('textarea[data-testid="commentary"]').value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: 'pending'
-    };
-    this.updateBill(bill);
-    this.onNavigate(ROUTES_PATH.Bills);
   };
 
   // not need to cover this function by tests
